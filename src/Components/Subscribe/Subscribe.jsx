599425d3b568c6c7e806bb7d35490bd713a3c44a
SiangYin <siangyin.work@gmail.com>
2026-04-19 19:16:24 +0800

    add subscribe

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import "./Subscribe.css";
+import React from "react"
+import "./Subscribe.css"
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <div className="subscribe section container">
+      <div className="secContainer grid">
+        <img
+          src={
+            "https://plus.unsplash.com/premium_photo-1661764534883-ce8b5d996ced?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          alt="Customer-care"
+          data-aos="fade-down"
+        />
+        <div className="textDiv">
+          <h4 data-aos="fade-up">Best Way To Start Your Journey!</h4>
+          <p data-aos="fade-up">
+            We offer personalized itineraries tailored to individual prefrences
+            and intrests.
+          </p>
+          <button className="btn" data-aos="fade-up">
+            Start Here
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
