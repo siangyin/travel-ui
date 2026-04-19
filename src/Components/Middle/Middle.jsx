58d85aa2bc1efe8ae8a20964f8e70e83565e78a7
SiangYin <siangyin.work@gmail.com>
2026-04-19 14:43:21 +0800

    add Middle and update App.scss

--- src/Components/Middle/Middle.jsx
+++ src/Components/Middle/Middle.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import "./Middle.css";
+import React from "react"
+import "./Middle.css"
 
 const Middle = () => {
   return (
-    <div>Middle</div>
+    <div className="middle section">
+      <div className="secContainer container">
+        <div className="grid">
+          <span className="flex" data-aos="fade-up">
+            <h1>10</h1>
+            <p>World of Experiences</p>
+          </span>
+
+          <span className="flex" data-aos="fade-up">
+            <h1>2K+</h1>
+            <p>Fine Destinations</p>
+          </span>
+
+          <span className="flex" data-aos="fade-up">
+            <h1>10K+</h1>
+            <p>Coustomer Reviewss</p>
+          </span>
+
+          <span className="flex" data-aos="fade-up">
+            <h1>4.8</h1>
+            <p>Overall Rating</p>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
