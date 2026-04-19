ab453b3304f26ebe333f42ba2ce883bfb52c5f67
SiangYin <siangyin.work@gmail.com>
2026-04-19 15:44:35 +0800

    add Destination component

--- src/Components/Destinations/Destinations.jsx
+++ src/Components/Destinations/Destinations.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
-import "./Destinations.css";
+import React from "react"
+import { MdLocationPin } from "react-icons/md"
+import { BsFillCreditCardFill } from "react-icons/bs"
+import { BsFillCalendarDateFill } from "react-icons/bs"
+import { BiSearchAlt } from "react-icons/bi"
+import { TiLocation } from "react-icons/ti"
+
+import "./Destinations.css"
+
+import { destinations } from "../../data/destination"
 
 const Destinations = () => {
   return (
-    <div>Destinations</div>
+    <div className="destination section container">
+      <div className="secContainer">
+        <div className="">
+          <span className="redText" data-aos="fade-up">
+            EXPLORE NOW
+          </span>
+          <h3 data-aos="fade-up">Find Your Dream Destinations</h3>
+          <p data-aos="fade-up">
+            Fill in the field below the best spot for your next tour.
+          </p>
+        </div>
+
+        <div className="searchField  grid">
+          <div className="inputField flex" data-aos="fade-up">
+            <MdLocationPin className="icon" />
+            <input type="text" placeholder="Location" />
+          </div>
+
+          <div className="inputField  flex" data-aos="fade-up">
+            <BsFillCreditCardFill className="icon" />
+            <input type="text" placeholder="Budget" />
+          </div>
+
+          <div className="inputField  flex" data-aos="fade-up">
+            <BsFillCalendarDateFill className="icon" />
+            <input type="text" placeholder="Date" />
+          </div>
+
+          <button className="btn flex" data-aos="fade-up">
+            <BiSearchAlt className="icon" />
+            Search
+          </button>
+        </div>
+
+        <div className="secMenu">
+          <ul className="flex" data-aos="fade-up">
+            <li className="active">All</li>
+            <li>Recommended</li>
+            <li>Beach</li>
+            <li>Park</li>
+            <li>Nature</li>
+            <li>Mountain</li>
+          </ul>
+        </div>
+
+        <div className="destinationContainer grid">
+          {destinations.map((destination) => {
+            return (
+              <div
+                className="singleDestination"
+                key={destination.id}
+                data-aos="fade-up"
+              >
+                <div className="imgDiv" data-aos="fade-up">
+                  <img src={destination.img} alt="" />
+                  <div className="desInfo flex">
+                    <div className="text">
+                      <span className="name">{destination.location}</span>
+                      <p className="flex">
+                        <TiLocation className="icon" />
+                        {destination.name}
+                      </p>
+                    </div>
+                    <span className="rating">{destination.rating}</span>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/data/destination.js
@@ -0,0 +1,99 @@
+export const destinations = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1548013146-72479768bada?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Taj Mahal",
+    location: "Agra, India",
+    rating: 4.7,
+  },
+
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1608037521244-f1c6c7635194?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "The Great Wall of China",
+    location: "Beijing, China",
+    rating: 4.2,
+  },
+
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1760779550145-8a9f5c4255dd?q=80&w=1562&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Chureito Pagoda and Mount Fuji",
+    location: "Fujiyoshida, Japan",
+    rating: 4.9,
+  },
+
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1643029891412-92f9a81a8c16?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Halong Bay",
+    location: "Ha Long, Vietnam",
+    rating: 4.5,
+  },
+
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1599283787923-51b965a58b05?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Angkor Wat",
+    location: "Krong Siem Reap, Cambodia",
+    rating: 4.2,
+  },
+
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1528181304800-259b08848526?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Wiang Kum Kam",
+    location: "Tha Wang Tan, Thailand",
+    rating: 4.4,
+  },
+
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1638964663550-e2123ac8900b?q=80&w=1750&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Gyeongbokgung Palace",
+    location: "Sajik-ro, Jongno-gu, Seoul, South Korea",
+    rating: 4.4,
+  },
+  // {
+  //   id: 8,
+  //   img: "https://images.unsplash.com/photo-1742539327294-0f3806573ae4?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //   name: "Tiger Nest Bhutan Monastery",
+  //   location: "Paro Taktsang, Bhutan",
+  //   rating: 4.4,
+  // },
+  {
+    id: 9,
+    img: "https://images.unsplash.com/photo-1490077476659-095159692ab5?q=80&w=1702&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Yangon, Myanmar",
+    location: "Yangon, Myanmar",
+    rating: 4.4,
+  },
+  {
+    id: 10,
+    img: "https://images.unsplash.com/photo-1503641926155-5c17619b79d0?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Gate of prayers",
+    location: "Tibet, China",
+    rating: 4.4,
+  },
+  {
+    id: 11,
+    img: "https://images.unsplash.com/photo-1711127016876-9a1b7e05126f?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Kyrgyzstan",
+    location: "Altai Mountains, Mongolia",
+    rating: 4.4,
+  },
+  {
+    id: 12,
+    img: "https://images.unsplash.com/photo-1531366936337-7c912a4589a7?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Aurora at Tromso",
+    location: "Tromso, Norway",
+    rating: 4.4,
+  },
+  {
+    id: 13,
+    img: "https://images.unsplash.com/photo-1604999333679-b86d54738315?q=80&w=1325&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Ulun Danu Beratan Temple",
+    location: "Bali, Indonesia",
+    rating: 4.4,
+  },
+]
