73bc01df1fa77932ac5416409a73d4e41faa099f
SiangYin <siangyin.work@gmail.com>
2026-04-19 19:31:27 +0800

    add footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
-import "./Footer.css";
+import React from "react"
+import { BiLogoMediumOld } from "react-icons/bi"
+import { ImFacebook } from "react-icons/im"
+import { BsTwitter } from "react-icons/bs"
+import { AiFillInstagram } from "react-icons/ai"
+
+import "./Footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="secContainer Container grid">
+        <div className="logoDiv">
+          <div className="footerLogo" data-aos="fade-up">
+            <BiLogoMediumOld className="icon" />
+            <span>angoGo Trip</span>
+          </div>
+          <div className="socials flex" data-aos="fade-up">
+            <ImFacebook className="icon" />
+            <BsTwitter className="icon" />
+            <AiFillInstagram className="icon" />
+          </div>
+        </div>
+
+        <div className="footerLinks" data-aos="fade-up">
+          <span className="linkTitle">Information</span>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <a href="#">Travel</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </div>
+        <div className="footerLinks" data-aos="fade-up">
+          <span className="linkTitle">Helpful Links</span>
+          <li>
+            <a href="#">Destination</a>
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+          <li>
+            <a href="#">Travel & Condition</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+        </div>
+        <div className="footerLinks" data-aos="fade-up">
+          <span className="linkTitle">Contact Details</span>
+          <span className="phone">+65 6655 6655</span>
+          <span className="email">contact@travel.com</span>
+          <li>
+            <a href="#">Home</a>
+            <a href="#">Explore</a>
+            <a href="#">travel</a>
+            <a href="#">Blog</a>
+          </li>
+        </div>
+      </div>
+    </div>
   )
 }
 
