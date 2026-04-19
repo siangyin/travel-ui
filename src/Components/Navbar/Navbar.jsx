7bd8588f6b913fe2e73a7b1dc35e0ecea470a0ae
SiangYin <siangyin.work@gmail.com>
2026-04-19 13:02:24 +0800

    navbar complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
+import { BiLogoMediumOld } from "react-icons/bi"
+import { AiFillCloseCircle } from "react-icons/ai"
+import { PiDotsNineBold } from "react-icons/pi"
 import "./Navbar.css"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const [navBar, SetNavBar] = useState("menu")
+
+  const showNavBar = () => {
+    SetNavBar("menu showNavbar")
+  }
+
+  const removeNavBar = () => {
+    SetNavBar("menu")
+  }
+
+  return (
+    <div className="navBar">
+      <div className="logoDiv">
+        <BiLogoMediumOld className="icon" />
+        <span>angoGo Trip</span>
+      </div>
+
+      <div className={navBar}>
+        <ul>
+          <li className="navList">Destination</li>
+          <li className="navList">About Us</li>
+          <li className="navList">Testimonial</li>
+          <li className="navList">Gallery</li>
+        </ul>
+
+        {/* Icon to close Navbar */}
+        <AiFillCloseCircle className="icon closeIcon" onClick={removeNavBar} />
+      </div>
+
+      <div className="action-btn">
+        <button className="signUpBtn btn">Sign Up</button>
+
+        {/* Icon to toggle Navbar */}
+        <PiDotsNineBold className="icon menuIcon" onClick={showNavBar} />
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
